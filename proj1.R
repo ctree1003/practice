9327ff2681498aed573c3cac99e21a1c67b7a9f5
ctree <ctree1003@gmail.com>
2018-10-04 10:39:01 +0900

    How
    서울시민만 뽑고싶다

--- proj1.R
+++ proj1.R
@@ -8,14 +8,16 @@ library(dplyr)
 library(ggplot2)
 library(data.table)#install.packages("data.table")
 
-demo <- read.table("D:/R/01.txt", header = T, 
-                   col.names = c("no", "sex", "age", "location"), 
-                   sep = ",", stringsAsFactors = F)
-purchase <- fread("D:/R/02.txt", header = T, sep = ",")
-product <- read.table("D:/R/03.txt", header = T, sep = ",", stringsAsFactors = F,
-                      col.names = c("co.com", "big.code", "mid.code", "small.code",
-                                    "mid.name", "small.name"))
+demo <- read.table("C:/Users/student/Documents/multi_chs/pro/01.txt", header = T, col.names = c("no", "sex", "age", "location"), sep = ",", stringsAsFactors = F)
+purchase <- fread("C:/Users/student/Documents/multi_chs/pro/02.txt", header = T, sep = ",")
+product <- read.table("C:/Users/student/Documents/multi_chs/pro/03.txt", header = T, sep = ",", 
+                      stringsAsFactors = F,
+                      col.names = c("co.com", "big.code", "mid.code","small.code","mid.name", "small.name"))
 table(demo$location)
 demo1 <- demo #복사본 생성
 demo1 <- subset(demo, location <= 99) #서울 지역인원만 추출
-demo1 #확인
+table(demo1$location) #확인
+
+table(demo1$no) #서울 시민만 뽑았으니 구매내역도 서울시민만 뽑고싶다
+
+head(purchase2)
