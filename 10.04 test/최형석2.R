72a0c817a950ed3445fe8a6d881285580fc02770
HS Choi <ctree1003@gmail.com>
2018-10-09 19:57:16 +0900

    수정 중

--- 10.04 test/최형석2.R
+++
@@ -1,7 +0,0 @@
-Classdata <- read.table("C:/Users/student/Documents/ClassData.txt", header = T, stringsAsFactors = T)
-set.seed(1809)
-Classdata <- subset(Classdata, select = -ID)
-
-sam <- sample(x=c("train","test"), size=nrow(Classdata), replace=T, prob = c(0.7,0.3))
-train <- Classdata[sam=="train",]
-test <- Classdata[sam=="test",]
